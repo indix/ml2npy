a6ade25fdd82b87188f657b4cf1d133fcb2bd8a2
Venkatesh <venkatesh@indix.com>
2016-12-27 15:03:03 +0530

    NpyFile implementation

--- src/main/scala/com/github/vumaasha/ml2npy/ml2npyCSR.scala
+++ src/main/scala/com/github/vumaasha/ml2npy/ml2npyCSR.scala
@@ -1,15 +1,15 @@
 package com.github.vumaasha.ml2npy
 
+import java.nio.ByteBuffer
+
 /**
   * Created by vumaasha on 25/12/16.
   */
 
-import scala.{specialized => spec}
-
-class ml2npyCSR[@spec(Double, Int, Float, Long, Short, Byte) V](data: Seq[V],
-                                                                indices: Seq[Int],
-                                                                indexPointers: Seq[Int],
-                                                                rows: Int,
-                                                                columns: Int) {
+class ml2npyCSR(data: Seq[Float], indices: Seq[Int], indexPointers: Seq[Int], rows: Int, columns: Int) {
+  val dataB: ByteBuffer = (new FloatNpyFile).addElements(data)
+  val indicesB: ByteBuffer = (new IntNpyFile).addElements(indices)
+  val indexPointersB: ByteBuffer = (new IntNpyFile).addElements(indexPointers)
+  val shapeB: ByteBuffer = (new IntNpyFile).addElements(Array(rows,columns))
 
 }
