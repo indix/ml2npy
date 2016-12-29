fa8db135e86463f503fe202dc5456a542fbccea0
Venkatesh <venkatesh@indix.com>
2016-12-29 15:37:54 +0530

    changes for supporting hadoop output format

--- src/main/scala/com/github/vumaasha/ml2npy/NpyFile.scala
+++ src/main/scala/com/github/vumaasha/ml2npy/NpyFile.scala
@@ -5,6 +5,8 @@ import java.nio.charset.StandardCharsets
 import java.nio.file.{Paths, StandardOpenOption}
 import java.nio.{ByteBuffer, ByteOrder}
 
+import scala.collection.mutable.ArrayBuffer
+
 
 /**
   * Created by vumaasha on 24/12/16.
@@ -20,6 +22,41 @@ abstract class NpyFile[V] {
    */
   val dtype: String
   val dataSize: Int
+  val dataBuffer: ArrayBuffer[Byte] = ArrayBuffer.empty[Byte]
+  var numElements:Int = 0
+
+  def getBytes:Array[Byte] = {
+    require(numElements >0, "Elements should be added to the buffer before getting bytes")
+    (getHeader(numElements) ++ dataBuffer).toArray
+  }
+
+  def getHeader(dataLength: Int): ArrayBuffer[Byte] = {
+
+    val header: ArrayBuffer[Byte] = ArrayBuffer()
+    val description = s"{'descr': '$dtype', 'fortran_order': False, 'shape': ($dataLength,), }"
+
+    val versionByteSize = 2
+    val headerByteSize = 2
+    val newLineSize = 1
+    val unpaddedLength: Int = magic.length + versionByteSize + headerByteSize + description.length + newLineSize
+    val isPaddingRequired = (unpaddedLength % 16) != 0
+    val paddingLength = (((unpaddedLength / 16) + 1) * 16) - unpaddedLength
+    val paddedDescription = if (isPaddingRequired) {
+      description + (" " * paddingLength)
+    } else description
+
+    val headerLength: Int = unpaddedLength + paddingLength
+    val content = ByteBuffer.allocate(headerLength)
+      .order(ByteOrder.LITTLE_ENDIAN)
+      .put(magic)
+      .put(majorVersion.toByte)
+      .put(minorVersion.toByte)
+      .putShort(paddedDescription.length.toShort)
+      .put(paddedDescription.getBytes(StandardCharsets.US_ASCII))
+
+    header ++ content.array()
+  }
+
 
   def addElements(data: Seq[V]) = {
     val dataLength = data.length.toString
@@ -49,15 +86,23 @@ abstract class NpyFile[V] {
     content
   }
 
-  def addElement(content: ByteBuffer)(elem: V): ByteBuffer
+  protected def addElement(content: ByteBuffer)(elem: V): ByteBuffer
+
+  def addToBuffer(elem:V):Unit = {
+    val buffer: ByteBuffer = ByteBuffer.allocate(dataSize)
+    dataBuffer ++= addElement(buffer)(elem).array()
+    numElements += 1
+  }
 }
 
 object NpyFile {
+
   implicit object IntNpyFile extends NpyFile[Int] {
     override val dtype: String = "<i4"
     override val dataSize: Int = 4
 
     override def addElement(content: ByteBuffer)(elem: Int) = content.putInt(elem)
+
   }
 
   implicit object LongNpyFile extends NpyFile[Long] {
@@ -71,15 +116,15 @@ object NpyFile {
     override val dtype: String = "<i2"
     override val dataSize: Int = 2
 
-  override def addElement(content: ByteBuffer)(elem: Short) = content.putShort(elem)
-}
+    override def addElement(content: ByteBuffer)(elem: Short) = content.putShort(elem)
+  }
 
   implicit object ByteNpyFile extends NpyFile[Byte] {
     override val dtype: String = "<i1"
     override val dataSize: Int = 1
 
-  override def addElement(content: ByteBuffer)(elem: Byte) = content.put(elem)
-}
+    override def addElement(content: ByteBuffer)(elem: Byte) = content.put(elem)
+  }
 
   implicit object FloatNpyFile extends NpyFile[Float] {
     override val dtype: String = "<f4"
@@ -107,7 +152,7 @@ object NpyTester {
     The imported matrix should contain the values (0.3,0.5)
      */
 
-    val content = NpyFile[Float].addElements(Seq(0.3f,0.5f))
+    val content = NpyFile[Float].addElements(Seq(0.3f, 0.5f))
     val channel = FileChannel.open(Paths.get("/tmp/test.npy"), StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE)
     channel.write(content)
     channel.close()
