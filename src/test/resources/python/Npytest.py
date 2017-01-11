de0f196dbe803de5b6a23db2d69dfe69b602f295
sasikanth <sasikanth@indix.com>
2017-01-11 15:00:07 +0530

    remove some hardcoded paths

--- src/test/resources/python/Npytest.py
+++ src/test/resources/python/Npytest.py
@@ -10,17 +10,9 @@ def test_1():
     print ('in should Convert a sequence of integers correctly')
     assert_true(np.allclose(np.load("/tmp/inttest.npy"),[1,2,3,4,5,6,7,8,9,10]))
 
-def test_2():
-    print ('in should Convert a sequence to non 1- indexed itegers correctly')
-    assert_true(np.allclose(np.load("/tmp/inttest2.npy"),[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]))
-
 def test_3():
 	print ('in should write bytes directly')
 	assert_true(np.allclose(np.load("/tmp/inttest3.npy"),[1,2,3,4,5,6,7,8,9,10]))
-
-def test_4():
-	print ('in should write bytes directly in a sequence')
-	assert_true(np.allclose(np.load("/tmp/inttest4.npy"),[11,12,13,14,15,16,17,18,19,20]))
 
 def test_5():
 	print ('in should work with csr matrix')
