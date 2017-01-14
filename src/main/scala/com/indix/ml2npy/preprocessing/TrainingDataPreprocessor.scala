c274c1cbee28a6d5abcc3f6cc555a18c8c761bf1
sasikanth@indix.com <Hendrix#3>
2017-01-14 16:28:23 +0530

    abstracted out the implementation of docgenerator

--- src/main/scala/com/indix/ml2npy/preprocessing/TrainingDataPreprocessor.scala
+++ src/main/scala/com/indix/ml2npy/preprocessing/TrainingDataPreprocessor.scala
@@ -26,131 +26,18 @@ trait DocGenerator {
     val numParts: Int = args(2).toInt
 
     val spark = SparkSession.builder().appName("Ml2Npy").getOrCreate()
-    prepare(spark, inputPath, outputPath, numParts)
+    val trainingRecords =readRecords(inputPath)
+    val toplevelTrainingRecords=sampleRecords(trainingRecords)
+    writeRecords(toplevelTrainingRecords,outputPath)
   }
 
   def tokenizer: PipelineStage
 
-  def prepare(spark: SparkSession, inputPath: String, outputPath: String, numParts: Int): Unit = {
-    import spark.implicits._
-    val trainingRecords: Dataset[TrainingRecord] = spark.read.json(inputPath).as[TrainingRecord].cache()
-    val toplevelTrainingRecords = trainingRecords.mapPartitions(iterator => {
-      val bcSelector = new Random(11)
-      val brandSelector = new Random(17)
-      for {
-        record <- iterator
-      } yield {
-        val isBc = bcSelector.nextInt(2) == 0
-        val isBrand = brandSelector.nextInt(2) == 0
-        val isSpecs = bcSelector.nextInt(2) == 0
-        val doc = {
-          record.title + {
-            if (isBrand) " " + record.brandText else " "
-          } + {
-            if (isBc) " " + record.breadCrumbs else " "
-          }
-        }
-        SimpleTrainingRecord(record.url, doc, Integer.parseInt(record.topLevelId).toShort, Integer.parseInt(record.leafId).toShort)
-      }
-    })
+  def pipeline: Pipeline
 
-    val docTokenizer = tokenizer
+  def readRecords(inputPath:String):Dataset[TrainingRecord]
 
-    val docCV = new CountVectorizer()
-      .setMinDF(100)
-      .setInputCol("tokens")
-      .setOutputCol("tokenCounts")
-      .setBinary(true)
+  def sampleRecords(trainingRecords:Dataset[TrainingRecord]):Dataset[SimpleTrainingRecord]
 
-    val docIDF = new IDF()
-      .setInputCol("tokenCounts")
-      .setOutputCol("tokenIDF")
-
-    val normalizer = new Normalizer()
-      .setInputCol("tokenIDF")
-      .setOutputCol("normalizedTokenVector")
-
-    val pipeline = new Pipeline()
-      .setStages(Array(docTokenizer, docCV, docIDF, normalizer))
-
-    val ppModel = pipeline.fit(toplevelTrainingRecords)
-    val transformedTopData = ppModel.transform(toplevelTrainingRecords)
-
-    transformedTopData.select("doc", "tokens", "normalizedTokenVector").show()
-
-    val idfData = transformedTopData.select("normalizedTokenVector", "topLevelId", "categoryId")
-      .map(x => (x.getAs[Vector](0), new DenseVector(Array(x.getShort(1), x.getShort(2)))))
-      .repartition(numParts)
-      .rdd
-      .saveAsHadoopFile(outputPath, classOf[Vector], classOf[Vector], classOf[NpyOutPutFormat])
-
-    val cvModel = ppModel.stages(1) match {
-      case x: CountVectorizerModel => x
-    }
-    val idfModel = ppModel.stages(2) match {
-      case x: IDFModel => x
-    }
-    val tfidf: TFIdfModel = TFIdfModel(cvModel.vocabulary, Some(idfModel.idf))
-    val data: Seq[TFIdfModel] = Seq(tfidf)
-    val tfidfDF = spark.createDataFrame[TFIdfModel](data).coalesce(1).toDF()
-    tfidfDF.write.json(s"$outputPath/tfidfModel")
-  }
+  def writeRecords(toplevelTrainingRecords: Dataset[SimpleTrainingRecord],outputPath:String)
 }
-
-/**
-  * Created by vumaasha on 28/12/16.
-  */
-object UnigramTokens extends DocGenerator {
-
-  override def tokenizer: PipelineStage = {
-    val docTokenizer = new RegexTokenizer()
-      .setGaps(false)
-      .setMinTokenLength(3)
-      .setToLowercase(true)
-      .setPattern("\\b[a-z0-9]+")
-      .setInputCol("doc")
-      .setOutputCol("tokens")
-    docTokenizer
-  }
-}
-
-object CooccTokens extends DocGenerator {
-
-  override def tokenizer: PipelineStage = {
-    val docTokenizer = new CooccurrenceTokenizer()
-      .setGaps(false)
-      .setMinTokenLength(3)
-      .setToLowercase(true)
-      .setPattern("\\b[a-z0-9]+")
-      .setInputCol("doc")
-      .setOutputCol("tokens")
-    docTokenizer
-  }
-}
-
-case class NgramTokenizer(n: Int) extends DocGenerator {
-
-  override def tokenizer: PipelineStage = {
-    val docTokenizer = new RegexTokenizer()
-      .setGaps(false)
-      .setMinTokenLength(3)
-      .setToLowercase(true)
-      .setPattern("\\b[a-z0-9]+")
-      .setInputCol("doc")
-      .setOutputCol("unigrams")
-
-    val nGrams = new NGram()
-      .setInputCol("unigrams")
-      .setOutputCol("tokens")
-      .setN(2)
-
-    val tokenPipeline = new Pipeline()
-    tokenPipeline.setStages(Array(docTokenizer, nGrams))
-    tokenPipeline
-  }
-}
-
-object BigramTokenizer extends NgramTokenizer(2)
-
-object TrigramTokenizer extends NgramTokenizer(3)
-
