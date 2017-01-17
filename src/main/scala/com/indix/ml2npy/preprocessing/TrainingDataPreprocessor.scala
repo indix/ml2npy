0ce2fb1f2e975b84dca70bed20d72588f007d230
sasikanth <sasikanth@indix.com>
2017-01-17 10:25:11 +0530

    abstracted out indix training record types

--- src/main/scala/com/indix/ml2npy/preprocessing/TrainingDataPreprocessor.scala
+++ src/main/scala/com/indix/ml2npy/preprocessing/TrainingDataPreprocessor.scala
@@ -1,23 +1,12 @@
 package com.indix.ml2npy.preprocessing
 
-import com.indix.ml2npy.hadoop.NpyOutPutFormat
-import com.indix.ml2npy.text.CooccurrenceTokenizer
-import org.apache.spark.ml.feature._
-import org.apache.spark.ml.linalg.{DenseVector, Vector}
-import org.apache.spark.ml.{Pipeline, PipelineStage}
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.sql.{Dataset, SparkSession}
 
 import scala.util._
 
-case class TrainingRecord(storeId: Long,
-                          url: String, title: String, breadCrumbs: String, brandText: String, categoryPath: String, price: Double, isbn: String, asin: String, specificationText: String, leafId: String,
-                          topLevelId: String, isBroken: Boolean, discoveredDate: Option[Long], discontinuedDate: Option[Long])
 
-case class SimpleTrainingRecord(url: String, doc: String, topLevelId: Short, categoryId: Short)
-
-case class TFIdfModel(vocab: Array[String], idf: Option[Vector])
-
-trait DocGenerator {
+trait DocGenerator[T,V] {
 
   def main(args: Array[String]): Unit = {
 
@@ -27,15 +16,15 @@ trait DocGenerator {
 
     val spark = SparkSession.builder().appName("Ml2Npy").getOrCreate()
     val trainingRecords =readRecords(inputPath)
-    val toplevelTrainingRecords=sampleRecords(trainingRecords)
-    writeRecords(toplevelTrainingRecords,outputPath,pipeline)
+    val sampledRecords=sampleRecords(trainingRecords)
+    writeRecords(sampledRecords,outputPath,pipeline)
   }
 
   def pipeline: Pipeline
 
   def readRecords(inputPath:String):Dataset[T]
 
-  def sampleRecords(trainingRecords:Dataset[T]):Dataset[T]
+  def sampleRecords(trainingRecords:Dataset[T]):Dataset[V]
 
-  def writeRecords(topLevelTrainingRecords: Dataset[T],outputPath:String,pipeline:Pipeline)
+  def writeRecords(sampledRecords: Dataset[V],outputPath:String,pipeline:Pipeline)
 }
